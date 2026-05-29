8818af43cf223959fc1696a7b907a9ffc37480e3
zlvvyy <z1lovvyy@gmail.com>
2026-05-29 21:34:26 +0800

    best  eval （delete）

--- projects/configs_e2e_univ2x/univ2x_coop_e2e_offline.py
+++
@@ -1,6 +0,0 @@
-_base_ = ['./univ2x_coop_e2e.py']
-
-# Step 2: run cooperative inference loading infrastructure track_instances from disk
-model_ego_agent = dict(
-    read_track_query_file_root='./inf_queries',
-)
