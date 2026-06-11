fa37a11b2208f485f28ff6b62379f78f3d86e6db
zlvvyy <z1lovvyy@gmail.com>
2026-06-11 15:00:15 +0800

    exp: add author checkpoint training with unfrozen bbox head (lr=5e-5, pts_bbox_head lr_mult=1.0)

---
+++ projects/configs_e2e_univ2x/univ2x_author_unfreeze_bboxhead.py
@@ -0,0 +1,25 @@
+_base_ = ['./univ2x_coop_e2e_lowlr_finetune.py']
+
+load_from = 'ckpts/univ2x_coop_e2e_stg2.pth'
+resume_from = None
+auto_resume = False
+
+work_dir = 'projects/work_dirs_e2e_univ2x/univ2x_author_unfreeze_bboxhead_lr5e5'
+
+optimizer = dict(
+    type='AdamW',
+    lr=5e-5,
+    weight_decay=0.01,
+    paramwise_cfg=dict(
+        custom_keys={
+            'img_backbone': dict(lr_mult=0.0),
+            'img_neck': dict(lr_mult=0.0),
+            'bev_encoder': dict(lr_mult=0.0),
+            'map_head': dict(lr_mult=0.0),
+            'motion_head': dict(lr_mult=0.0),
+            'planning_head': dict(lr_mult=0.0),
+            'cross_agent_query_interaction': dict(lr_mult=1.0),
+            'pts_bbox_head': dict(lr_mult=1.0),
+        }
+    )
+)
